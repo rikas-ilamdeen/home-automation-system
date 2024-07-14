e6ecf7e4b5f7e0ca064547e5f8be1b25838c95b5
Rikas Ilamdeen <rikasilamdeen@gmail.com>
2024-07-14 13:45:47 +0530

    comments

--- README.md
+++ README.md
@@ -2,13 +2,30 @@
 Control your home devices remotely with a web interface. This project includes a Flask-based web server on Raspberry Pi.
 
 ### Features
-
 - **Web Interface:** Control connected devices through a user-friendly web page.
 - **Raspberry Pi Integration:** The Flask-based web server runs on a Raspberry Pi for local control.
 
 #### Requirements
-
 - Raspberry Pi with GPIO support
 - Relay module (if controlling high-power devices)
 - Python installed on Raspberry Pi
 - Flask Python library
+
+#### Running the Project
+1. Open a terminal on your Raspberry Pi.
+2. Navigate to the project directory.
+    ```bash
+    cd path/to/rpi-led-control
+    ```
+3. Run the Flask application.
+    ```bash
+    python app.py
+    ```
+4. Open a web browser and go to `http://<your_pi_ip>:5000` to access the LED control interface.
+5. Use the provided buttons to turn the LED on and off.
+6. To stop the Flask application, return to the terminal and press `Ctrl+C` to interrupt the process.
+
+#### Improve
+- Mobile app (android and ios)
+- connect many devices (high voltage)
+- NodeMCU ESP32 (low cost)
--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import RPi.GPIO as GPIO
-from flask import Flask, render_template, request, redirect, url_for
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO     # library for working with GPIO pins on the Raspberry Pi
+from flask import Flask, render_template, request, redirect, url_for  # classes and functions from the Flask library
 
-app = Flask(__name__)
+app = Flask(__name__)       # Flask application setup
 
 # Disable GPIO warnings
 GPIO.setwarnings(False)
 
 # Set up GPIO mode and channel
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)  # Assuming you are using GPIO 17
+GPIO.setup(17, GPIO.OUT)
 
-@app.route("/")
-def home():
+@app.route("/")     
+def home():                     # Defines a route for the home page
     return render_template("index.html")
 
 @app.route("/on")
@@ -25,5 +24,5 @@ def turn_off():
     GPIO.output(17, GPIO.LOW)  # Turn the LED off
     return "LED turned off!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":          # Checks if the script is being run directly (not imported as a module)
+    app.run(host="0.0.0.0", port=5000, debug=True)      # starts the Flask application on host 
